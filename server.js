adf6f6d0b6d50adcb8ae1320a80c15b677404750
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-10-27 09:12:55 -0400

    Fixed port typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
   , fs   = require('fs')
   , url  = require('url')
-  , port = 9090
+  , port = 8080;
 
 var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
@@ -18,7 +18,7 @@ var server = http.createServer (function (req, res) {
   }
 })
 
-server.listen(9090)
+server.listen(port)
 console.log('listening on 8080')
 
 // subroutines
